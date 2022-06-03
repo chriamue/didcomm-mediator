d3918632547b9b7529a631b11619e0329ef134a2
Christian M <chriamue@gmail.com>
2022-06-03 16:47:13 +0200

    :sparkles: added well known did to worker

--- didcomm-mediator-worker/src/lib.rs
+++ didcomm-mediator-worker/src/lib.rs
@@ -1,5 +1,5 @@
 use base58::FromBase58;
-use did_key::{generate, DIDCore, X25519KeyPair, CONFIG_JOSE_PUBLIC};
+use did_key::{generate, DIDCore, X25519KeyPair, CONFIG_JOSE_PUBLIC, CONFIG_LD_PUBLIC};
 use didcomm_mediator::invitation::Invitation;
 use serde_json::json;
 use worker::*;
@@ -37,6 +37,22 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
                     ext_service
                 ),
             }))
+        })
+        .get("/.well-known/did.json", |_req, ctx| {
+            let seed = ctx.secret("SEED").unwrap().to_string();
+            let ext_service = ctx.var("EXT_SERVICE").unwrap().to_string();
+            let key = generate::<X25519KeyPair>(Some(&seed.from_base58().unwrap()));
+            let mut did_doc = key.get_did_document(CONFIG_LD_PUBLIC);
+            did_doc.verification_method[0].private_key = None;
+            let mut did_doc = serde_json::to_value(&did_doc).unwrap();
+            did_doc["service"] = serde_json::json!([
+              {
+                "id": "2e9e814a-c1e1-416e-a21a-a4182809950c",
+                "serviceEndpoint": ext_service,
+                "type": "did-communication"
+              }
+            ]);
+            Response::from_json(&did_doc)
         })
         .run(req, env)
         .await
